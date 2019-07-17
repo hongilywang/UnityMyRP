c1ba7877101b6e4aeff7c521524359e67f36fbaf
hongilywang <hongilywang@sina.com>
2019-07-17 20:35:03 +0800

    将每个相机分别渲染skybox，并且将相机属性传入shader，得到正确的skybox渲染效果

--- Assets/MyRP/Runtime/MyPipeline.cs
+++ Assets/MyRP/Runtime/MyPipeline.cs
@@ -10,8 +10,16 @@ namespace MyRP
     {
         protected override void Render(ScriptableRenderContext context, Camera[] cameras)
         {
-            context.DrawSkybox(cameras[0]);
-            context.Submit();
+            for (int i = 0; i < cameras.Length; ++i)
+                Render(context, cameras[i]);
+        }
+
+        void Render(ScriptableRenderContext renderContext, Camera camera)
+        {
+            //将相机的属性（比如相机的视口矩阵）出入shader
+            renderContext.SetupCameraProperties(camera);
+            renderContext.DrawSkybox(camera);
+            renderContext.Submit();
         }
     }
 }
