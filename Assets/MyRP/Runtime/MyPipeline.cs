e588ca2a3e194ddddb21c6c947fce049426cd6d3
hongilywang <hongilywang@sina.com>
2019-07-17 20:07:47 +0800

    通过MyPipelineAsset创建一个MyPipeline的asset文件，然后在MyPipeline的render接口中实现skybox的渲染

---
+++ Assets/MyRP/Runtime/MyPipeline.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace MyRP
+{
+    public class MyPipeline : RenderPipeline
+    {
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            context.DrawSkybox(cameras[0]);
+            context.Submit();
+        }
+    }
+}
