1a99aa3e9aff3c8f0495ba68599d1e18ca385eee
hongilywang <hongilywang@sina.com>
2019-07-17 20:54:44 +0800

    硬编码一个commandBuffer，然后清除RT的深度值，保留颜色

--- Assets/MyRP/Runtime/MyPipeline.cs
+++ Assets/MyRP/Runtime/MyPipeline.cs
@@ -8,18 +8,27 @@ namespace MyRP
 {
     public class MyPipeline : RenderPipeline
     {
-        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        protected override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
         {
             for (int i = 0; i < cameras.Length; ++i)
-                Render(context, cameras[i]);
+                Render(renderContext, cameras[i]);
         }
 
-        void Render(ScriptableRenderContext renderContext, Camera camera)
+        void Render(ScriptableRenderContext context, Camera camera)
         {
             //将相机的属性（比如相机的视口矩阵）出入shader
-            renderContext.SetupCameraProperties(camera);
-            renderContext.DrawSkybox(camera);
-            renderContext.Submit();
+            context.SetupCameraProperties(camera);
+
+            //创建command buffer
+            var commandBuffer = new CommandBuffer();
+            commandBuffer.name = "CommandBufferBeforeSkybox";
+            //清除RT的深度值，保留颜色
+            commandBuffer.ClearRenderTarget(true, false, Color.clear);
+            context.ExecuteCommandBuffer(commandBuffer);
+            commandBuffer.Release();
+
+            context.DrawSkybox(camera);
+            context.Submit();
         }
     }
 }
