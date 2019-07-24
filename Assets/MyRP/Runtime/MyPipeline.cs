adc3a46f99c9c4b0e421f6a53ca3fa47c8f7daa3
hongilywang <hongilywang@sina.com>
2019-07-24 19:17:47 +0800

    Lit Shader，在MyPipeline中传入最多4个可见的方向光的颜色和方向数据

--- Assets/MyRP/Runtime/MyPipeline.cs
+++ Assets/MyRP/Runtime/MyPipeline.cs
@@ -22,8 +22,17 @@ namespace MyRP
         //是否开启gup instance
         bool enableGPUInstancing;
 
+        //将方向光的颜色和方向传入shader
+        const int maxVisibleLights = 4;
+        static int visibleLightColorsId = Shader.PropertyToID("_VisibleLightColors");
+        static int visibleLightDirectionsId = Shader.PropertyToID("_VisibleLightDirections");
+        Vector4[] visibleLightColors = new Vector4[maxVisibleLights];
+        Vector4[] visibleLightDirections = new Vector4[maxVisibleLights];
+
         public MyPipeline(bool dynamicBatching, bool instancing)
         {
+            //light的强度值使用线性空间
+            GraphicsSettings.lightsUseLinearIntensity = true;
             enableDynamicBatching = dynamicBatching;
             enableGPUInstancing = instancing;
         }
@@ -53,7 +62,16 @@ namespace MyRP
             //根据相机设置来清理RT
             CameraClearFlags clearFlags = camera.clearFlags;
             cameraBuffer.ClearRenderTarget((clearFlags & CameraClearFlags.Depth) != 0, (clearFlags & CameraClearFlags.Color) != 0, camera.backgroundColor);
+
+            //获取可见的方向光
+            ConfigureLights();
+
             cameraBuffer.BeginSample(commandBufferName);
+
+            //将方向光的颜色和方向传入shader
+            cameraBuffer.SetGlobalVectorArray(visibleLightColorsId, visibleLightColors);
+            cameraBuffer.SetGlobalVectorArray(visibleLightDirectionsId, visibleLightDirections);
+
             context.ExecuteCommandBuffer(cameraBuffer);
             cameraBuffer.Clear();
 
@@ -101,6 +119,21 @@ namespace MyRP
             drawingSettings.overrideMaterial = errorMaterial;
             FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.all, -1);
             context.DrawRenderers(culling, ref drawingSettings, ref filteringSettings);
+        }
+
+        //存入可见的方向光信息
+        void ConfigureLights()
+        {
+            for (int i = 0; i < culling.visibleLights.Length; ++i)
+            {
+                VisibleLight light = culling.visibleLights[i];
+                visibleLightColors[i] = light.finalColor;
+                Vector4 v = light.localToWorldMatrix.GetColumn(2);
+                v.x = -v.x;
+                v.y = -v.y;
+                v.z = -v.z;
+                visibleLightDirections[i] = v;
+            }
         }
     }
 }
---
+++ Assets/MyRP/ShaderLibrary/Lit.hlsl
@@ -0,0 +1,82 @@
+#ifndef MYRP_LIT_INCLUDED
+#define MYRP_LIT_INCLUDED
+
+#include "com.unity.render-pipelines.core@5.16.1/ShaderLibrary/Common.hlsl"
+
+CBUFFER_START(UnityPerFrame) 
+    float4x4 unity_MatrixVP;
+CBUFFER_END
+
+CBUFFER_START(UnityPerDraw)
+    float4x4 unity_ObjectToWorld;
+CBUFFER_END
+
+#define MAX_VISIBLE_LIGHT 4
+
+CBUFFER_START(_LightBuffer)
+    float4 _VisibleLightColors[MAX_VISIBLE_LIGHT];
+    float4 _VisibleLightDirections[MAX_VISIBLE_LIGHT];
+CBUFFER_END
+
+float3 DiffuseLight (int index, float3 normal)
+{
+    float3 lightColor = _VisibleLightColors[index].rgb;
+    float3 lightDirection = _VisibleLightDirections[index].xyz;
+    float diffuse = saturate(dot(normal, lightDirection));
+    return diffuse * lightColor;
+}
+
+#define UNITY_MATRIX_M unity_ObjectToWorld
+#include "com.unity.render-pipelines.core@5.16.1/ShaderLibrary/UnityInstancing.hlsl"
+
+// CBUFFER_START(UnityPerMaterial)
+//     float4 _Color;
+// CBUFFER_END
+
+//相同材质，不同的颜色属性也可以使用GPU Instance一次绘制
+UNITY_INSTANCING_BUFFER_START(PerInstance)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _Color)
+UNITY_INSTANCING_BUFFER_END(PerInstance)
+
+struct VertexInput 
+{
+    float4 pos : POSITION;
+    float3 normal : NORMAL;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct VertexOutput 
+{
+    float4 clipPos : SV_POSITION;
+    float3 normal : TEXCOORD0;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+VertexOutput LitPassVertex (VertexInput input)
+{
+    VertexOutput output;
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_TRANSFER_INSTANCE_ID(input, output);
+    float4 worldPos = mul(UNITY_MATRIX_M, float4(input.pos.xyz, 1.0));
+    output.clipPos = mul(unity_MatrixVP, worldPos);
+    output.normal = mul((float3x3)UNITY_MATRIX_M, input.normal);
+    return output;
+}
+
+float4 LitPassFragment (VertexOutput input) : SV_TARGET
+{
+    UNITY_SETUP_INSTANCE_ID(input);
+    input.normal = normalize(input.normal);
+    float3 albedo = UNITY_ACCESS_INSTANCED_PROP(PerInstance, _Color).rgb;
+
+    float3 diffuseLight = 0;
+    for (int i = 0; i < MAX_VISIBLE_LIGHT; i++)
+    {
+        diffuseLight += DiffuseLight(i, input.normal);
+    }
+
+    float3 color = diffuseLight * albedo;
+    return float4(color, 1);
+}
+
+#endif // MY_LIT_INCLUDED
